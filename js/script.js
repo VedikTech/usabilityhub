7fc4be08d7ee5ca4e5e59b1754b406c7504e141e
vedikathote <vedikathote@gmail.com>
2024-12-19 19:55:29 +0530

    added some responsiveness

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
-let navbar=document.getElementById("navbar");
-let cross=document.getElementById("cross");
+let navbar = document.getElementById("navbar");
+let cross = document.getElementById("cross");
+let navitems = document.querySelector(".navitembox");
+let body = document.body; 
+let html =document.html;
 
-navbar.addEventListener("click" , ()=>{
-    
-})
+navbar.addEventListener("click", function () {
+  navitems.classList.add("active");
+  cross.style.display = "block";
+  navbar.style.display = "none";
+  body.classList.add('no-scroll');
+});
+
+cross.addEventListener("click", function () {
+  navitems.classList.remove("active");
+  cross.style.display = "none";
+  navbar.style.display = "block";
+  body.classList.remove('no-scroll');
+});
